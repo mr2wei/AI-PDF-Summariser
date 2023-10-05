24d4a03deda21ab2b6b998dbe3a162ec114e68fb
Wei Jiang <weijiang@Weis-MacBook-Air.local>
2023-10-05 09:14:31 -0600

    added a feedback form, still need to make a back end

--- src/App.js
+++ src/App.js
@@ -4,12 +4,37 @@ import React from 'react';
 import Home from './pages/Home.js';
 
 function App() {
+
+  
+  
+  function sendFeedback() {
+    //
+  }
   
   return (
     <div className="App">
 
       <Home />
-
+      
+      <footer>
+        <h3>This tool is still a <b>work in progress</b></h3>
+        <p>Please leave some feedback on potential improvements</p>
+        <form onSubmit={sendFeedback}>
+          <label>
+            Feedback title:
+            <br />
+            <input type="text" name="feedback-title" placeholder='title' />
+          </label>
+          <br />
+          <label>
+            Feedback:
+            <br />
+            <textarea name="feedback" placeholder='feedback' />
+          </label>
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+      </footer>
 
     </div>
   );
